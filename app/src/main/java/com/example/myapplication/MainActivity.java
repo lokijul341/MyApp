2156f68444950d6e0d22e16623f941f00d4e2925
User <rnltmst@mail.ru>
2023-02-24 10:40:26 +0300

    Добавление Log'ов

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton myButton = findViewById(R.id.imageButton5);
+        myButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Log.i("MyApp", "Программный способ");
+            }
+        });
+
+    }
+
+    public void onMyButtonClick(View view){
+        Log.i("MyApp", "Декларативный способ");
     }
 }
