82ebc75c4495ac358b268bf905bb5fb381b4a779
Арина <rnltmst@mail.ru>
2023-03-10 23:11:57 +0300

    Добавлено второе Activity с registerForActivityResult

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,21 +21,24 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     private ImageButton imageButton ;
 
+    ActivityResultLauncher<Intent> activityLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView13);
-
         ImageButton button = findViewById(R.id.imageButton7);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 String text = ((TextView) findViewById(R.id.textView13)).getText().toString();
                 intent.putExtra("text", text);
-                startActivityForResult(intent, REQUEST_CODE);
+                activityLauncher.launch(intent);
             }
         });
 
@@ -42,16 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("MyApp", "Уведомления включены!");
             }
         });
+        activityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+                            String resultText = data.getStringExtra("text");
+                            textView.setText(resultText);
+                        }
+                    }
+                });
     }
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-            String result = data.getStringExtra("text");
-            TextView textView = findViewById(R.id.textView13);
-            textView.setText(result);
-        }
 
-    }
     public void onMyButtonClick(View view){
         Log.i("MyApp", "Уведомления включены!");
     }
